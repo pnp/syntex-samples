8cf75f53900a2e1c67c28936587393df4aa3323b
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2022-10-05 15:28:20 +0100

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -19,11 +19,11 @@ docfx build ./main/docs/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
 
-Remove-Item ./gh-pages/articles/* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item ./gh-pages/contributing/* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item ./gh-pages/models/* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item ./gh-pages/scenario-samples/* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item ./gh-pages/scripts/* -Recurse -Force -ErrorAction SilentlyContinue
-Remove-Item ./gh-pages/site-templates/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/articles/* -Recurse -Force 
+Remove-Item ./gh-pages/contributing/* -Recurse -Force 
+Remove-Item ./gh-pages/models/* -Recurse -Force 
+Remove-Item ./gh-pages/scenario-samples/* -Recurse 
+Remove-Item ./gh-pages/scripts/* -Recurse -Force 
+Remove-Item ./gh-pages/site-templates/* -Recurse 
 copy-item -Force -Recurse ./main/docs/_site/* -Destination ./gh-pages
 
