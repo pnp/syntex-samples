d65b8698f2d72ade2160f127ba6cbf38eb94097f
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2022-10-05 13:33:10 +0100

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -11,11 +11,12 @@ foreach($folder in $foldersToMoveToDocs)
   copy-item -Force ./main/$folder/* -Destination ./main/docs/$folder -Recurse
 
 }
-
+<#
 docfx metadata ./main/docs/docfx.json --warningsAsErrors $args
 docfx build ./main/docs/docfx.json --warningsAsErrors $args
-
+#>
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
+<#
 Remove-Item ./gh-pages/articles/* -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item ./gh-pages/contributing/* -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item ./gh-pages/models/* -Recurse -Force -ErrorAction SilentlyContinue
@@ -23,3 +24,4 @@ Remove-Item ./gh-pages/scenario-samples/* -Recurse -Force -ErrorAction SilentlyC
 Remove-Item ./gh-pages/scripts/* -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item ./gh-pages/site-templates/* -Recurse -Force -ErrorAction SilentlyContinue
 copy-item -Force -Recurse ./main/docs/_site/* -Destination ./gh-pages
+#>
