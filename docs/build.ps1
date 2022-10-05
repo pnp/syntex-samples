56b1bccb161656ad7f8fca8868e9dc9884a07359
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2022-10-05 13:20:03 +0100

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -16,10 +16,10 @@ docfx metadata ./main/docs/docfx.json --warningsAsErrors $args
 docfx build ./main/docs/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
-Remove-Item ./gh-pages/articles/* -Recurse -Force
-Remove-Item ./gh-pages/contributing/* -Recurse -Force
-Remove-Item ./gh-pages/models/* -Recurse -Force
-Remove-Item ./gh-pages/scenario-samples/* -Recurse -Force
-Remove-Item ./gh-pages/scripts/* -Recurse -Force
-Remove-Item ./gh-pages/site-templates/* -Recurse -Force
+Remove-Item ./gh-pages/articles/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/contributing/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/models/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/scenario-samples/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/scripts/* -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item ./gh-pages/site-templates/* -Recurse -Force -ErrorAction SilentlyContinue
 copy-item -Force -Recurse ./main/docs/_site/* -Destination ./gh-pages
