dd37f14a43f0cd95bae3592fa49373687319fcec
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2022-10-05 14:49:44 +0100

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -10,7 +10,7 @@ foreach($folder in $foldersToMoveToDocs)
   #Get-Item -Path "./main/docs"
   New-Item -Path "./main/docs" -Name $folder -ItemType Directory -Force
 
-  Copy-Item -Force ./main/$folder/* -Destination ./main/docs/$folder -Recurse
+  Copy-Item -Force ./main/$folder/* -Destination ./main/docs/$folder -Recurse 
 
 }
 
